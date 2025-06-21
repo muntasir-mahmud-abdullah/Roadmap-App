da2bed279390916c15e44530c9a3631e2383b2fb
MuntasirMahmud <muntasirm525@gmail.com>
2025-06-21 12:24:25 +0600

    backend connection with database changed to mongoose in seperate file for easy access and cleaner code

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,1 +1,17 @@
 const mongoose = require('mongoose');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.werzz.mongodb.net/roadmapData?retryWrites=true&w=majority&appName=Cluster0`;
+
+// mongoose.connect(uri);
+
+const connectDb = async () => {
+    try {
+        await mongoose.connect(uri);
+        console.log("connection successful to DB");
+
+    } catch (error) {
+        console.log("Database connection failed");
+        process.exit(0);
+    }
+}
+
+module.exports = connectDb;
