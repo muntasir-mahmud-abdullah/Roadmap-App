51e49acf661cdf3c4dc0f10617c2be3a6ca65bb7
MuntasirMahmud <muntasirm525@gmail.com>
2025-06-22 19:37:24 +0600

    frontend: input handling done for signup and login form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,81 @@
 import React from 'react';
-
+import { useState } from "react";
 const Login = () => {
-    return (
-        <div>
-            <h1>This is Login page</h1>
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  // handling the input values
+  const handleInput = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
+// handling the form submission
+const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user);
+}
+  return (
+    <>
+      <section>
+        <div className="">
+          <div className="">
+            {/* image section */}
+            <div className="">
+              <img
+                src="images/dp-removebg-preview.png"
+                alt=""
+                className="w-48 h-40"
+              />
+            </div>
+          </div>
+          {/* registration form section */}
+          <div className="">
+            <h1 className="">Login form</h1>
+            <br />
+            <form action="" onSubmit= {handleSubmit}>
+              <div className="">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Enter Your Email"
+                  id=""
+                  required
+                  autoComplete="off"
+                  value={user.email}
+                  onChange={handleInput}
+                />
+              </div>
+              <div>
+                <label htmlFor="password">Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Enter Your Password"
+                  id=""
+                  required
+                  autoComplete="off"
+                  value={user.password}
+                  onChange={handleInput}
+                />
+              </div>
+              <br />
+              <button className="" type="submit">
+                Login Now
+              </button>
+            </form>
+          </div>
         </div>
-    );
+      </section>
+    </>
+  );
 };
 
 export default Login;
