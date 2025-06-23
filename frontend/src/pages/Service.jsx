bbf7b34e760dc1a1f1834c0d2f02e05b85996203
MuntasirMahmud <muntasirm525@gmail.com>
2025-06-24 02:13:33 +0600

    frontend: roadmap items data fetched to frontend from backend

--- frontend/src/pages/Service.jsx
+++ frontend/src/pages/Service.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
-
+import { useAuth } from "../store/Auth";
+import {useEffect} from 'react';
 const Service = () => {
-    return (
-        <section>
-            <div className="container">
-                <h1 className="text-2xl">Services</h1>
-            </div>
-        </section>
+  const { services } = useAuth();
+      return (
+      <section>
+        <div className="container">
+          <h1 className="text-2xl">{services.length} Services</h1>
+        </div>
+        <div className="container grid grid-cols-3">
+          {services.map((service) => {
+          return  <div className="card border-2 m-4 p-4" key={service._id}>
+              <h1>{service.title}</h1>
+              <p>{service.description}</p>
+              <p>{service.category}</p>
+              <p>{service.status}</p>
+              <p>{service.upvoteCount}</p>
+            </div>;
+          })}
+        </div>
+      </section>
     );
 };
 
--- frontend/src/store/Auth.jsx
+++ frontend/src/store/Auth.jsx
@@ -3,6 +3,7 @@ export const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
   const [token, setToken] = useState(localStorage.getItem("token"));
   const [user, setUser] = useState("");
+  const [services,setServices] = useState([]);
   const storeTokenInLS = (serverToken) => {
     setToken(serverToken);
     return localStorage.setItem("token", serverToken);
@@ -39,14 +40,28 @@ const AuthProvider = ({ children }) => {
         console.log("error fetching user data");
       }
     };
-
+    // fetch services data from database
+    const getServices = async() => {
+      try {
+        const response = await fetch("http://localhost:5000/api/data/service",{
+          method:"GET",
+        });
+        if(response.ok) {
+          const data = await response.json();
+          setServices(data.msg);
+        }
+      } catch (error) {
+        console.log("services frontend error:", error)
+      }
+    }
     useEffect(()=>{
+      getServices();
       userAuthentication();
     },[token])
 
   return (
     <AuthContext.Provider
-      value={{ isLoggedIn, storeTokenInLS, logoutUser, user }}
+      value={{ isLoggedIn, storeTokenInLS, logoutUser, user, services }}
     >
       {children}
     </AuthContext.Provider>
