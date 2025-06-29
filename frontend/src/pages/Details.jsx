08555e878795154695361639fdbdd54ce09248d5
MuntasirMahmud <muntasirm525@gmail.com>
2025-06-29 11:21:48 +0600

    fixed: frontend details page error fixed

--- frontend/src/pages/Details.jsx
+++ frontend/src/pages/Details.jsx
@@ -1,16 +1,16 @@
-import { useParams, Link } from "react-router";
+import { useState } from "react";
+import { Link, useParams } from "react-router";
 import { useAuth } from "../store/Auth";
 const details = () => {
+  const [comments, setcomments] = useState([]);
+  const [item, setItem] = useState(null);
+  const [editingCommentId, setEditingCommentId] = useState(null);
+  const [editingContent, setEditingContent] = useState("");
   const { services } = useAuth();
   let params = useParams();
   const serviceItem = services.find((service) => service._id === params.id);
-  const {_id,title,description,category,status,upvotesCount} = serviceItem;
-
-  const [serviceItem,setServicesItem] = useState(null);
-  const [comments,setcomments] = useState([]);
-  const [editingCommentId,setEditingCommentId] = useState(null);
-  const [editingContent,setEditingContent] = useState("");
-  const userId =
+  const { _id, title, description, category, status, upvotesCount } =
+    serviceItem;
   return (
     <div className="flex flex-col gap-4 items-center">
       <h1>{title}</h1>
@@ -19,7 +19,7 @@ const details = () => {
       <p>Status: {status}</p>
       <p>Upvotes : {upvotesCount}</p>
       <Link to="/service" className="btn-primary">
-          Back To Services
+        Back To Services
       </Link>
     </div>
   );
