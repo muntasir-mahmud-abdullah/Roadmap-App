0863238199a5982a17b6503246ee073940a6ef22
MuntasirMahmud <muntasirm525@gmail.com>
2025-06-29 15:51:39 +0600

    fronend: comment successfully fetched to service details page

--- frontend/src/pages/Details.jsx
+++ frontend/src/pages/Details.jsx
@@ -5,11 +5,23 @@ const details = () => {
   const [comments, setcomments] = useState([]);
   const [editingCommentId, setEditingCommentId] = useState(null);
   const [editingContent, setEditingContent] = useState("");
-  const { services, user, userId } = useAuth();
+  const { services, user,API,token, userId } = useAuth();
   let params = useParams();
   const serviceItem = services.find((service) => service._id === params.id);
   const { _id, title, description, category, status, upvotesCount } =
     serviceItem;
+  
+  console.log(_id)
+  const handleCommentAdded = async() => {
+    const commentsResponse = await fetch(`${API}/api/auth/service/${_id}/comments`,{
+      method:"GET",
+      headers: {Authorization:`Bearer ${token}`}
+    });
+    const data = await commentsResponse.json();
+    console.log(data)
+
+  }
+  handleCommentAdded()
   return (
     <div className="flex flex-col gap-4 items-center">
       <h1>{title}</h1>
